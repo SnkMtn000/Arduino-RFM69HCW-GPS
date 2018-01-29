898c2f69f35b3bdc651e21923e035cbaa996d541
SnkMtn000 <cewebbusa@gmail.com>
2018-01-29 10:38:45 -0500

    Add files via upload
    All of these compiled, uploaded and executed from an arduino prom mini 3.3v, Hope RFM69HCW and Neo-6 u-blox  GPS

---
+++ qst2_promini33_rfm69_FELD_HELL_Keyboard_20180116.ino
@@ -0,0 +1,267 @@
+/*
+ * A simple Feld Hell sender
+ * for the HopeRF rfm69hcw
+ * on a ProMini 3.3
+ * Charles Webb
+ * KC1ENN@arrl.net
+ * 
+ * with thanks to James Coxon
+ * 
+ * Feld Hell for HopeRF rfm69hcw modifed from source below
+ * https://github.com/jamescoxon/PicoAtlas/blob/master/Pico6MK3/sketch_apr02b/sketch_apr02b.ino
+ * 
+ * 
+ */
+
+#include <SPI.h>
+
+#define MY_DELAY 5000  // delay in loop, between sending the message
+#define ssPin 10  //  SS
+#define rstPin 9 //  reset pin
+//#define radioPower 0x58  // 6 dBm
+//#define radioPower 0x5B  //  9 dBm
+//#define radioPower 0x5E  // 12 dBm
+#define radioPower 0x7F  // 17 dBm
+
+char mytext[80];
+String inputString = "";         // a String to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+long myFrequency = 433200000;
+//long myFrequency = 915000000;
+String myString = "";
+String myCall = "... CQ   CQ   CQ   KC1ENN   KC1ENN   KC1ENN   PSK ...    ";
+int myStringLen = 0;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Starting up");
+  pinMode(ssPin, OUTPUT);
+  pinMode(rstPin, OUTPUT);
+  setupSPI();
+  delay(1000);
+  resetRFM69();
+  delay(1000);
+  setupRFM69();
+}
+ 
+void loop (){
+  if (stringComplete) {
+  // clear the string:
+  inputString = "";
+  stringComplete = false;
+  }
+} //ends void loop
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+      if(inputString.indexOf("@freq=") == 0) {
+        myFrequency = inputString.substring(6,15).toInt();
+        setFrequency(myFrequency);
+        Serial.print("Frequency now set to: "); Serial.println(inputString.substring(6,15));
+        inputString = "";
+      }
+      if(inputString.indexOf("@freq+") == 0) {
+        myFrequency = myFrequency + 61;
+        setFrequency(myFrequency);
+        Serial.print("Frequency now set to: "); Serial.println(myFrequency);
+        inputString = "";
+      } 
+      if(inputString.indexOf("@freq-") == 0) {
+        myFrequency = myFrequency - 61;
+        setFrequency(myFrequency);
+        Serial.print("Frequency now set to: "); Serial.println(myFrequency);
+        inputString = "";
+      }
+      if (inputString.indexOf("@cq") == 0) {
+        myString = myCall;
+        myString.toUpperCase();
+        myStringLen = myString.length();
+        myString.toCharArray(mytext,myString.length() + 1);
+        Serial.print("I am sending: "); Serial.println(myString);
+        hellsendmsg(mytext);
+      } 
+      else {
+        inputString.trim();
+          if(inputString.length() > 0) {
+          myString = "..." + inputString + " ...     ";
+          myString.toUpperCase();
+          myStringLen = myString.length();
+          myString.toCharArray(mytext,myString.length() + 1);
+          Serial.print("I am sending: "); Serial.println(myString);
+          hellsendmsg(mytext);
+        }
+      }
+    }
+  }
+}
+
+void setupRFM69() {
+  writeReg(0x02,0x34);  //  Modulation for OOK, Continuous w.o bit synch, OOK, no shaping
+  setFrequency(myFrequency);
+  Serial.print("Frequency set to "); Serial.println(myFrequency);
+  writeReg(0x26,0x07); // CLK off to save power
+  writeReg(0x01,0x0C);  // Set mode to Transmitter TX mode
+  Serial.println("Transmitter is ready....");
+}
+
+// set the frequency (in Hz)
+void setFrequency(uint32_t freqHz){
+  freqHz /= 61; // divide down by FSTEP to get FRF
+  writeReg(0x07, freqHz >> 16);
+  writeReg(0x08, freqHz >> 8);
+  writeReg(0x09, freqHz);
+}
+
+void resetRFM69() {
+  digitalWrite(rstPin, HIGH);
+  delay(100);
+  digitalWrite(rstPin, LOW);
+  delay(100);
+}
+
+void printReg(byte data) {
+  Serial.print("Register "); 
+  Serial.print(data); 
+  Serial.print(" = "); 
+  Serial.println(readReg(data), HEX);
+}
+
+void writeReg(uint8_t addr, uint8_t value) {
+  digitalWrite(ssPin,LOW);
+  SPI.transfer(addr | 0x80);
+  SPI.transfer(value);
+  digitalWrite(ssPin, HIGH);
+}
+
+uint8_t readReg(uint8_t addr) {
+  digitalWrite(ssPin, LOW);
+  SPI.transfer(addr & 0x7F);
+  uint8_t regval = SPI.transfer(0);
+  digitalWrite(ssPin, HIGH);
+  return regval;
+}
+
+void setupSPI() {
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+}
+
+//***************Hellschreiber******************
+ 
+struct t_htab { char c; int hellpat[5]; } ;
+ 
+struct t_htab helltab[] = {
+ 
+  {'1', { B00000100, B00000100, B01111100, B00000000, B00000000 } },
+  {'2', { B01001000, B01100100, B01010100, B01001100, B01000000 } },
+  {'3', { B01000100, B01000100, B01010100, B01010100, B00111100 } },
+  {'4', { B00011100, B00010000, B00010000, B01111100, B00010000 } },
+  {'5', { B01000000, B01011100, B01010100, B01010100, B00110100 } },
+  {'6', { B00111100, B01010010, B01001010, B01001000, B00110000 } },
+  {'7', { B01000100, B00100100, B00010100, B00001100, B00000100 } },
+  {'8', { B00111100, B01001010, B01001010, B01001010, B00111100 } },
+  {'9', { B00001100, B01001010, B01001010, B00101010, B00111100 } },
+  {'0', { B00111000, B01100100, B01010100, B01001100, B00111000 } },
+  {'A', { B01111000, B00101100, B00100100, B00101100, B01111000 } },
+  {'B', { B01000100, B01111100, B01010100, B01010100, B00101000 } },
+  {'C', { B00111000, B01101100, B01000100, B01000100, B00101000 } },
+  {'D', { B01000100, B01111100, B01000100, B01000100, B00111000 } },
+  {'E', { B01111100, B01010100, B01010100, B01000100, B01000100 } },
+  {'F', { B01111100, B00010100, B00010100, B00000100, B00000100 } },
+  {'G', { B00111000, B01101100, B01000100, B01010100, B00110100 } },
+  {'H', { B01111100, B00010000, B00010000, B00010000, B01111100 } },
+  {'I', { B00000000, B01000100, B01111100, B01000100, B00000000 } },
+  {'J', { B01100000, B01000000, B01000000, B01000000, B01111100 } },
+  {'K', { B01111100, B00010000, B00111000, B00101000, B01000100 } },
+  {'L', { B01111100, B01000000, B01000000, B01000000, B01000000 } },
+  {'M', { B01111100, B00001000, B00010000, B00001000, B01111100 } },
+  {'N', { B01111100, B00000100, B00001000, B00010000, B01111100 } },
+  {'O', { B00111000, B01000100, B01000100, B01000100, B00111000 } },
+  {'P', { B01000100, B01111100, B01010100, B00010100, B00011000 } },
+  {'Q', { B00111000, B01000100, B01100100, B11000100, B10111000 } },
+  {'R', { B01111100, B00010100, B00010100, B00110100, B01011000 } },
+  {'S', { B01011000, B01010100, B01010100, B01010100, B00100100 } },
+  {'T', { B00000100, B00000100, B01111100, B00000100, B00000100 } },
+  {'U', { B01111100, B01000000, B01000000, B01000000, B01111100 } },
+  {'V', { B01111100, B00100000, B00010000, B00001000, B00000100 } },
+  {'W', { B01111100, B01100000, B01111100, B01000000, B01111100 } },
+  {'X', { B01000100, B00101000, B00010000, B00101000, B01000100 } },
+  {'Y', { B00000100, B00001000, B01110000, B00001000, B00000100 } },
+  {'Z', { B01000100, B01100100, B01010100, B01001100, B01100100 } },
+  {'.', { B01000000, B01000000, B00000000, B00000000, B00000000 } },
+  {',', { B10000000, B10100000, B01100000, B00000000, B00000000 } },
+  {'/', { B01000000, B00100000, B00010000, B00001000, B00000100 } },
+  {'*', { B00000000, B00000000, B00000100, B00001110, B00000100 } }
+ 
+};
+ 
+#define N_HELL  (sizeof(helltab)/sizeof(helltab[0]))
+ 
+void helldelay() {
+  //Slow Hell
+  //delay(64);
+  //delayMicroseconds(900);
+ 
+  //Feld-Hell
+  delay(8);  //  was 8 and hands off
+  delayMicroseconds(100);  // was 160  used to adjust slope of displayed text
+}
+ 
+void on() {
+  writeReg(0x11,radioPower); // PA On
+  helldelay();
+  writeReg(0x11,0x00);
+}
+
+void hellsend(char c) {
+  int i ;
+  if (c == ' ') {
+      for (int d=0; d<14; d++){
+        helldelay();  
+      }
+    return ;
+  }
+  for (i=0; i<N_HELL; i++) {
+    if (helltab[i].c == c) {
+      //Serial.print(helltab[i].c) ;
+ 
+      for (int j=0; j<=4; j++) 
+      {
+        byte mask = B10000000;
+        for (int q=0; q<=6; q++)
+        {      
+          if(helltab[i].hellpat[j] & mask) {
+            on();
+          } else {
+            helldelay();
+          }
+          mask >>= 1;
+        }
+      }
+      for (int d=0; d<14; d++){
+        helldelay();  
+      }
+      return ;
+    }
+  }
+  /* if we drop off the end, then we send a space */
+  //Serial.print("?") ;
+}
+ 
+void hellsendmsg(char *str) {
+  //flag = 1;
+  delay(1000);
+  while (*str)
+    hellsend(*str++) ;
+  //Serial.println("");
+  //flag = 0;
+}
---
+++ qst2_promini33_rfm69_Morse_Beacon_20180121.ino
@@ -0,0 +1,224 @@
+/*
+ * A simple GPS Morse Beacon Sender
+ * for the HopeRF rfm69hcw
+ * on a ProMini 3.3
+ * Charles Webb
+ * KC1ENN@arrl.net
+ * 
+ * With thanks to....
+ * 
+ * "This code is so trivial that I'm releasing it completely without restrictions."
+ * Simple Arduino Morse Beacon v0.0
+ * Written by Mark VandeWettering <k6hx@arrl.net>
+ * Check out my blog @ http://brainwagon.org
+ */
+ 
+#include <SPI.h>
+
+#define ssPin 10 // SS  Chip select pin for reading/writing to the rf69 registers with SPI
+//#define ssPin 8  // adafruit feather M0
+#define rstPin 9 //  reset pin
+//#define rstPin 4 // adafruit feather M0
+#define MY_DELAY 3000  // 3 second delay between loops, sending the message
+#define N_MORSE (sizeof(morsetab)/sizeof(morsetab[0]))
+#define SPEED (15) // WORKS 5 - 50 wpm FL-DIGI
+#define DOTLEN (1200/SPEED)
+#define DASHLEN (3*(1200/SPEED))
+//#define MY_TX_POWER 0x50  // PA0 only range 0x50 -18dBm to 0x5F -3dBm
+//#define MY_TX_POWER 0x58  // 6 dBm
+//#define MY_TX_POWER 0x5B  //  9 dBm
+//#define MY_TX_POWER 0x5E  // 12 dBm
+//#define MY_TX_POWER 0x70 //  2 dbm
+#define MY_TX_POWER 0x7F  // 17 dBm
+
+char mytext[80];
+String inputString = "";         // a String to hold incoming data
+String myCallSign = "CALLSIGN/B TEST BEACON"; //  Put more than callsign here, put your beacon string here
+long myFrequency = 433200000;
+//long myFrequency = 915000000;
+String myString = "";
+String myCall = ". CALLSIGN/B";
+int myStringLen = 0;
+unsigned int count=0;
+//char mycount = 0;
+int myHour, myMinute, mySecond, myAlt, mySpd, mySat;
+double myLat, myLng;
+
+void setup() {
+  Serial.begin(9600);   // baud speed for sending to the arduino serial monitor
+  Serial.println("Starting up");  // print to the serial monitor
+  pinMode(ssPin, OUTPUT);  // define
+  pinMode(rstPin, OUTPUT);
+  setupSPI();
+  delay(1000);
+  resetRFM69();
+  delay(1000);
+  setupRFM69();
+}
+
+void loop() {
+  count++;
+  buildString();
+  //myString = myCall;
+        myString.toUpperCase();
+        myStringLen = myString.length();
+        myString.toCharArray(mytext,myString.length() + 1);
+     //   Serial.print("I am sending: "); Serial.println(myString);
+       sendmsg(mytext);
+  inputString = "";
+  myString = "";
+}
+
+void buildString() {
+  myString = myCallSign;
+  myString.toUpperCase();
+  myStringLen = myString.length();
+  Serial.print("I am sending: "); Serial.println(myString);
+}
+
+void setupRFM69() {
+  writeReg(0x02,0x34);  //  Modulation for OOK, Continuous w.o bit synch, OOK, no shaping
+  setFrequency(myFrequency);
+  writeReg(0x01,0x0C);  // Set mode to Transmitter TX mode
+  Serial.println("Transmitter is ready....");
+}
+
+struct t_mtab { char c, pat; } ;
+
+struct t_mtab morsetab[] = {
+  {'.', 106},
+  {',', 115},
+  {'?', 76},
+  {'/', 41},
+  {'A', 6},
+  {'B', 17},
+  {'C', 21},
+  {'D', 9},
+  {'E', 2},
+  {'F', 20},
+  {'G', 11},
+  {'H', 16},
+  {'I', 4},
+  {'J', 30},
+  {'K', 13},
+  {'L', 18},
+  {'M', 7},
+  {'N', 5},
+  {'O', 15},
+  {'P', 22},
+  {'Q', 27},
+  {'R', 10},
+  {'S', 8},
+  {'T', 3},
+  {'U', 12},
+  {'V', 24},
+  {'W', 14},
+  {'X', 25},
+  {'Y', 29},
+  {'Z', 19},
+  {'1', 62},
+  {'2', 60},
+  {'3', 56},
+  {'4', 48},
+  {'5', 32},
+  {'6', 33},
+  {'7', 35},
+  {'8', 39},
+  {'9', 47},
+  {'0', 63}
+} ;
+
+void resetRFM69() {
+  digitalWrite(rstPin, HIGH);
+  delay(100);
+  digitalWrite(rstPin, LOW);
+  delay(100);
+}
+
+void printReg(byte data) {
+  Serial.print("Register "); 
+  Serial.print(data); 
+  Serial.print(" = "); 
+  Serial.println(readReg(data), HEX);
+}
+
+void writeReg(uint8_t addr, uint8_t value) {
+  digitalWrite(ssPin,LOW);
+  SPI.transfer(addr | 0x80);
+  SPI.transfer(value);
+  digitalWrite(ssPin, HIGH);
+}
+
+uint8_t readReg(uint8_t addr) {
+  digitalWrite(ssPin, LOW);
+  SPI.transfer(addr & 0x7F);
+  uint8_t regval = SPI.transfer(0);
+  digitalWrite(ssPin, HIGH);
+  return regval;
+}
+
+void setupSPI() {
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+}
+
+void setFrequency(uint32_t freqHz){  // Setup the frequency
+  freqHz /= 61; // resolution is 61 Hz so divide it it down and mask it out
+  writeReg(0x07, freqHz >> 16);   // Frequency MSB
+  writeReg(0x08, freqHz >> 8);    // Frequency Middle Byte
+  writeReg(0x09, freqHz);         // Frequency LSB
+  Serial.print("Frequency set to "); Serial.println(myFrequency);
+}
+
+void txOn(int txTime) {
+  writeReg(0x11,MY_TX_POWER); // PA On *******************************************
+  delay(txTime);
+  writeReg(0x11,0x00); // PA off *************************************************
+}
+
+void dash() {
+  txOn(DASHLEN);
+  delay(DOTLEN);
+}
+
+void dit() {
+  txOn(DOTLEN);
+  delay(DOTLEN);
+}
+
+void send(char c) {
+  int i ;
+  if (c == ' ') {
+    //Serial.print(c) ;
+    delay(7*DOTLEN) ;
+    return ;
+  }
+  for (i=0; i<N_MORSE; i++) {
+    if (morsetab[i].c == c) {
+      unsigned char p = morsetab[i].pat ;
+      //Serial.print(morsetab[i].c) ;
+      while (p != 1) {
+          if (p & 1)
+            dash() ;
+          else
+            dit() ;
+          p = p / 2 ;
+      }
+      //tone(cwPin,500,2*DOTLEN);
+      delay(2*DOTLEN) ;
+      return ;
+    }
+  }
+  /* if we drop off the end, then we send a space */
+  //Serial.print("?") ;
+}
+
+void sendmsg(char *str) {
+  //send('.');  uncomment for fldigi
+  while (*str)
+    send(*str++) ;
+  //Serial.println("");
+}
+
+
